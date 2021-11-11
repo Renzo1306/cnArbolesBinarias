61b5d8dae5ee7d660f7fa5e206c6974eb3cddd75
Sebas6422 <91403654+Sebas6422@users.noreply.github.com>
2021-11-11 15:18:59 -0500

    Update Program.cs

--- cnArbolesBinarias/Program.cs
+++ cnArbolesBinarias/Program.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace cnArbolesBinarias
 {
+    class Nodo
+    {
+        public int nro;
+        public Nodo izq;
+        public Nodo der;
+        
+        public Nodo(int valor)
+        {
+            nro = valor;
+            izq = null;
+            der = null;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
